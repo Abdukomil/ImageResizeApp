a779ee943b092bf5a674f7c69a1ec7d54b181b0d
Abdukomil Rakhmatullaev <60283783+Abdukomil@users.noreply.github.com>
2023-03-02 18:30:32 +0900

    Initial Commit

---
+++ ImageResizeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageResizeApp
+//
+//  Created by abdukomil on 2023/03/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
